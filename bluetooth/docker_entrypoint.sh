09719cf26dc8b0274387bfad66e5f139b095b7ba
Bruno Costa <brunosccosta@gmail.com>
2022-02-26 14:18:21 +0000

    Using scan library to send mqtt packages

--- bluetooth/docker_entrypoint.sh
+++ bluetooth/docker_entrypoint.sh
@@ -2,20 +2,13 @@
 
 set -e
 
-export CRON=${CRON:-"* * * * *"}
-
 service dbus start
 bluetoothd &
 
-echo "$CRON /usr/local/bin/python3.7 /app/data-read.py >> /var/log/cron.log 2>&1" > /etc/cron.d/data-read
-
-cat /etc/cron.d/data-read
-
-# Apply cron job
-crontab /etc/cron.d/data-read
-
-# Create the log file to be able to run tail
-touch /var/log/cron.log
-
-# cat /var/spool/cron/crontabs/root
-cron && tail -f /var/log/cron.log
+/usr/local/bin/python3 /app/LYWSD03MMC.py --atc \
+	--influxdb 1 \
+	--watchdogtimer 5 \
+	--mqttconfigfile /conf/mqtt.ini \
+	--devicelistfile /conf/devices.ini \
+	--onlydevicelist \
+	--battery
